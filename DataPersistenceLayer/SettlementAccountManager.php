443cbb398781709d765cd1c46b1bd6fb7aaac1f1
WU XIAO <xwu9@ualberta.ca>
2015-03-03 21:12:05 -0700

    MAIN SUB SETTLE 核心API
    具体接口要根据那边微调 不过框架写好了

--- DataPersistenceLayer/SettlementAccountManager.php
+++ DataPersistenceLayer/SettlementAccountManager.php
@@ -86,30 +86,81 @@
             
         }
         
-        function InsertData($dataJson)
-        {
+        function UpdateData($data){
+            
             include_once('HTTP/Request.php');
             
-            $return=$dataJson;
-            return $return;
+            //$data="tablename=MainTable&state=1&通道=CTP&经纪公司=海通期货&经纪公司服务器=上海电信&账户ID=888&账户密码=111";
             
-            /*$data = array("tablename" => "SettlementAccount");
-             $data_string = json_encode($data);
-             $ch = curl_init('http://121.40.57.186/SPService/SPService.svc/loadtabledata');
-             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-             curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-             'Content-Type: application/json',
-             'Content-Length: ' . strlen($data_string)));
-             
-             $result = curl_exec($ch);
-             curl_close($ch);
-             header("Content-type:text/html;charset=utf-8");
-             
-             return $result;*/
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'http://121.40.57.186/SPService/SPService.svc/updatetablerowdata');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                                       'Content-Type: x-www-form-urlencoded',
+                                                       'Content-Length: 129' ));
+            
+            $result = curl_exec($ch);
+            
+            curl_close($ch);
+            header("Content-type:text/html;charset=utf-8");
+            
+            return strlen($result);
             
         }
+        
+        
+        function InsertData($data){
+            
+            include_once('HTTP/Request.php');
+            //$data="tablename=MainTable&state=1&通道=CTP&经纪公司=海通期货&经纪公司服务器=上海电信&账户ID=888&账户密码=111";
+            
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'http://121.40.57.186/SPService/SPService.svc/updatetablerowdata');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                                       'Content-Type: x-www-form-urlencoded',
+                                                       'Content-Length: 129' ));
+            
+            $result = curl_exec($ch);
+            
+            curl_close($ch);
+            header("Content-type:text/html;charset=utf-8");
+            
+            return strlen($result);
+            
+        }
+        
+        
+        function DeleteData($data)
+        {
+            include_once('HTTP/Request.php');
+            //$data="tablename=MainTable&state=1&通道=CTP&经纪公司=海通期货&经纪公司服务器=上海电信&账户ID=888&账户密码=111";
+            
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'http://121.40.57.186/SPService/SPService.svc/updatetablerowdata');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                                       'Content-Type: x-www-form-urlencoded',
+                                                       'Content-Length: 129' ));
+            
+            $result = curl_exec($ch);
+            
+            curl_close($ch);
+            header("Content-type:text/html;charset=utf-8");
+            
+            return strlen($result);
+            
+        }
+        
         
         function GetSettlementAccountNo()
         {
--- DataPersistenceLayer/SubAccountManager.php
+++ DataPersistenceLayer/SubAccountManager.php
@@ -34,6 +34,82 @@
             return $result;
         }
         
+        function UpdateData($data){
+            
+            include_once('HTTP/Request.php');
+            
+            //$data="tablename=MainTable&state=1&通道=CTP&经纪公司=海通期货&经纪公司服务器=上海电信&账户ID=888&账户密码=111";
+            
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'http://121.40.57.186/SPService/SPService.svc/updatetablerowdata');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                                       'Content-Type: x-www-form-urlencoded',
+                                                       'Content-Length: 129' ));
+            
+            $result = curl_exec($ch);
+            
+            curl_close($ch);
+            header("Content-type:text/html;charset=utf-8");
+            
+            return strlen($result);
+            
+        }
+        
+        
+        function InsertData($data){
+            
+            include_once('HTTP/Request.php');
+            //$data="tablename=MainTable&state=1&通道=CTP&经纪公司=海通期货&经纪公司服务器=上海电信&账户ID=888&账户密码=111";
+            
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'http://121.40.57.186/SPService/SPService.svc/updatetablerowdata');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                                       'Content-Type: x-www-form-urlencoded',
+                                                       'Content-Length: 129' ));
+            
+            $result = curl_exec($ch);
+            
+            curl_close($ch);
+            header("Content-type:text/html;charset=utf-8");
+            
+            return strlen($result);
+            
+        }
+        
+        
+        function DeleteData($data)
+        {
+            include_once('HTTP/Request.php');
+            //$data="tablename=MainTable&state=1&通道=CTP&经纪公司=海通期货&经纪公司服务器=上海电信&账户ID=888&账户密码=111";
+            
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'http://121.40.57.186/SPService/SPService.svc/updatetablerowdata');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                                       'Content-Type: x-www-form-urlencoded',
+                                                       'Content-Length: 129' ));
+            
+            $result = curl_exec($ch);
+            
+            curl_close($ch);
+            header("Content-type:text/html;charset=utf-8");
+            
+            return strlen($result);
+            
+        }
+        
+
         function GetMainAccountNo()
         {
             $hardcode=1;
