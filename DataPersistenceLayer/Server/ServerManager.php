b6acea571fb7a3fa015f73646f647e450e6aa8aa
WU XIAO <xwu9@ualberta.ca>
2015-04-04 13:14:29 -0600

    重启服务器PHP
    重启 和开启 都在SERVER底下

--- DataPersistenceLayer/Server/ServerManager.php
+++ DataPersistenceLayer/Server/ServerManager.php
@@ -124,8 +124,49 @@
             
         }
         
+        function ConnectAllSPServerMainAccounts(){
+            include_once('HTTP/Request.php');
+            //$next="abc";
+            //return $next;
+            $data = array("port" => "10083");
+            $data_string = json_encode($data);
+            $ch = curl_init('121.40.131.144/Test/SPService/SPService.svc/ConnectAllSPServerMainAccounts');
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                                       'Content-Type: application/json',
+                                                       'Content-Length: ' . strlen($data_string)));
+            
+            $result = curl_exec($ch);
+            curl_close($ch);
+            header("Content-type:text/html;charset=utf-8");
+            
+            return $result;
+            
+        }
         
-        
+        function RestartSharpSpeedServer(){
+            include_once('HTTP/Request.php');
+            //$next="abc";
+            //return $next;
+            $data = array("port" => "10083");
+            $data_string = json_encode($data);
+            $ch = curl_init('121.40.131.144/Test/SPService/SPService.svc/RestartSharpSpeedServer');
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                                       'Content-Type: application/json',
+                                                       'Content-Length: ' . strlen($data_string)));
+            
+            $result = curl_exec($ch);
+            curl_close($ch);
+            header("Content-type:text/html;charset=utf-8");
+            
+            return $result;
+            
+        }
 
     } // end of class Vegetable
 
