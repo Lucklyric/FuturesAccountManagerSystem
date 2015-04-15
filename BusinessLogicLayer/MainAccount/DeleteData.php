8fc5add225553bc238f22c5c083f2335cc4f44bf
shuyi <bugdx123@gmail.com>
2015-04-14 23:35:24 -0600

    TODO: 风控：UI 入金：取数据 主账户：自动选择

--- BusinessLogicLayer/MainAccount/DeleteData.php
+++ BusinessLogicLayer/MainAccount/DeleteData.php
@@ -1,65 +1,64 @@
+
 <?php
 
-//include "_SERVER['DOCUMENT_ROOT']/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccountManager.php";
-// utility functions
-$path = "../../..";
-//echo $path."/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccountManager.php";
-include $path . "/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccount/MainRowClass.php";
-include $path . "/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccount/MainAccountManager.php";
-include $path . "/FuturesAccountManagerSystem/DataPersistenceLayer/SubAccount/SubRowClass.php";
-include $path . "/FuturesAccountManagerSystem/DataPersistenceLayer/SubAccount/SubAccountManager.php";
+    //include "_SERVER['DOCUMENT_ROOT']/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccountManager.php";
+    // utility functions
+    $path = "../../..";
+    //echo $path."/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccountManager.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccount/MainRowClass.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccount/MainAccountManager.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/SubAccount/SubRowClass.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/SubAccount/SubAccountManager.php";
 
-$MainId = isset($_GET["MainId"]) ? $_GET["MainId"] : "";
-$Channel = isset($_GET["Channel"]) ? $_GET["Channel"] : "";
-$CompanyName = isset($_GET["CompanyName"]) ? $_GET["CompanyName"] : "";
-$CompanyServer = isset($_GET["CompanyServer"]) ? $_GET["CompanyServer"] : "";
-$AccountId = isset($_GET["AccountId"]) ? $_GET["AccountId"] : "";
-$AccountPassword = isset($_GET["AccountPassword"]) ? $_GET["AccountPassword"] : "";
-$StaticEquity = isset($_GET["StaticEquity"]) ? $_GET["StaticEquity"] : "";
-$State = isset($_GET["State"]) ? $_GET["State"] : "";
-$TableName = isset($_GET["TableName"]) ? $_GET["TableName"] : "";
-//编号
-$RowId = isset($_GET["RowId"]) ? $_GET["RowId"] : "";
+    $MainId=isset($_GET["MainId"]) ? $_GET["MainId"] :"";
+    $Channel=isset($_GET["Channel"]) ? $_GET["Channel"] :"";
+    $CompanyName=isset($_GET["CompanyName"]) ? $_GET["CompanyName"] :"";
+    $CompanyServer=isset($_GET["CompanyServer"]) ? $_GET["CompanyServer"] :"";
+    $AccountId=isset($_GET["AccountId"]) ? $_GET["AccountId"] :"";
+    $AccountPassword=isset($_GET["AccountPassword"]) ? $_GET["AccountPassword"] :"";
+    $StaticEquity=isset($_GET["StaticEquity"]) ? $_GET["StaticEquity"] :"";
+    $State=isset($_GET["State"]) ? $_GET["State"] :"";
+    $TableName=isset($_GET["TableName"]) ? $_GET["TableName"] :"";
+    //编号
+    $RowId=isset($_GET["RowId"]) ? $_GET["RowId"] :"";
 
-$AdminAccount = isset($_GET["AdminAccount"]) ? $_GET["AdminAccount"] : "";
-$AdminPassword = isset($_GET["AdminPassword"]) ? $_GET["AdminPassword"] : "";
+    $AdminAccount=isset($_GET["AdminAccount"]) ? $_GET["AdminAccount"] :"";
+    $AdminPassword=isset($_GET["AdminPassword"]) ? $_GET["AdminPassword"] :"";
 
-$MaxMoney = isset($_GET["MaxMoney"]) ? $_GET["MaxMoney"] : "";
+    $MaxMoney=isset($_GET["MaxMoney"]) ? $_GET["MaxMoney"] :"";
 
-$TableName = "MainAccount";
-$AdminAccount = "frankzch";
-$AdminPassword = "123456";
-$State = "2";
+    $TableName="MainAccount";
+    $AdminAccount="frankzch";
+    $AdminPassword="123456";
+    $State="2";
 
-$Channel = "CTP";
-$CompanyName = "游云模拟";
-$CompanyServer = "模拟test";
-$AccountId = "00044";
-$AccountPassword = "3";
-$StaticEquity = "0.1";
-$MaxMoney = "200";
-$RowId = "6";
-//$data="";
-if ($TableName && $State && $Channel && $CompanyName && $CompanyServer && $AccountId && $AccountPassword && $MaxMoney) {
-    $data = "admin=" . $AdminAccount . "&password=" . $AdminPassword . "&state=" . $State . "&编号=" . $RowId . "&通道=" .
-        $Channel . "&经纪公司名称=" . $CompanyName . "&账户ID=" . $AccountId . "&账户密码=" . $AccountPassword;
-     echo $data;
-} else {
+    $Channel="CTP";
+    $CompanyName="游云模拟";
+    $CompanyServer="模拟test";
+    $AccountId="00044";
+    $AccountPassword="3";
+    $StaticEquity="0.1";
+    $MaxMoney="200";
+    //$data="";
+    if($TableName && $State && $Channel && $CompanyName && $CompanyServer && $AccountId && $AccountPassword && $MaxMoney){
+        $data="admin=".$AdminAccount."&password=".$AdminPassword."&tablename=".$TableName."&state=".$State."&编号=".
+            $RowId;
+    }else{
 
-    echo "Some data is missing!";
+        echo "Some data is missing!";
 
-}
+    }
 
-//echo $data;
-echo "<br>";
+    //echo $data;
+    echo "<br>";
 
-$testAccount = new MainAccountManager();
-$SubAccount = new SubAccountManager();
-if ($data) {
+    $testAccount = new MainAccountManager();
+    $SubAccount = new SubAccountManager();
+    if($data){
 
-    $response = $testAccount->InsertData($data);
+        $response=$testAccount->InsertData($data);
 
-}
-echo($response);
+    }
+    echo($response);
 
-?>
+    ?>
---
+++ BusinessLogicLayer/MainAccount/InsertData_backup.php
@@ -0,0 +1,57 @@
+
+<?php
+    
+    //include "_SERVER['DOCUMENT_ROOT']/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccountManager.php";
+    // utility functions
+    $path = $_SERVER['DOCUMENT_ROOT']."/webclient.csapp";
+    // $path = "localhost/webclient.csapp";
+
+    //echo $path."/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccountManager.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/MainAccountManager.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/SubAccountManager.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/SettlementAccountManager.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/MainRowClass.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/SubRowClass.php";
+    include $path."/FuturesAccountManagerSystem/DataPersistenceLayer/SettlementRowClass.php";
+    
+    $MainId=isset($_GET["MainId"]) ? $_GET["MainId"] :"";
+    $Channel=isset($_GET["Channel"]) ? $_GET["Channel"] :"";
+    $CompanyName=isset($_GET["CompanyName"]) ? $_GET["CompanyName"] :"";
+    $CompanyServer=isset($_GET["CompanyServer"]) ? $_GET["CompanyServer"] :"";
+    $AccountId=isset($_GET["AccountId"]) ? $_GET["AccountId"] :"";
+    $AccountPassword=isset($_GET["AccountPassword"]) ? $_GET["AccountPassword"] :"";
+    $StaticEquity=isset($_GET["StaticEquity"]) ? $_GET["StaticEquity"] :"";
+    $State=isset($_GET["State"]) ? $_GET["State"] :"";
+    $TableName=isset($_GET["TableName"]) ? $_GET["TableName"] :"";
+
+    $TableName="MainTable";
+    $State="1";
+    // $Channel="2";
+    // $CompanyName="海通期货";
+    // $CompanyServer="test";
+    // $AccountId="2";
+    // $AccountPassword="3";
+    $initialdata="tablename=";
+    $data="";
+    if($TableName && $State && $Channel && $CompanyName && $CompanyServer && $AccountId && $AccountPassword){
+    $data=$initialdata.$TableName."&state=".$State."&通道=".$Channel."&经纪公司=".$CompanyName."&经纪公司服务器=".$CompanyServer."&账户ID=".$AccountId."&账户密码=".$AccountPassword;
+        echo $data;
+    }else{
+    
+        echo "Some data is missing!";
+    
+    }
+    
+    //echo $data;
+    echo "<br>";
+    
+    $testAccount = new MainAccountManager();
+    $SubAccount = new SubAccountManager();
+    if($data){
+        
+        $response=$testAccount->InsertData($data);
+    
+    }
+    echo($response);
+    
+    ?>
